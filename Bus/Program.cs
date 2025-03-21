d0769492a998650524544aa4fa931a837a1d04ed
ashuriel <155574754+ashuriel@users.noreply.github.com>
2025-03-20 23:14:43 -0400

    Update Program.cs

--- Bus/Program.cs
+++ Bus/Program.cs
@@ -1,1 +1,27 @@
-﻿
+using System;
+
+namespace Bus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CentralBuses central = new CentralBuses();
+
+            
+            Bus bus1 = new BusPlatinum(101);
+            Bus bus2 = new BusGold(102);
+
+            
+            bus1.AgregarPasajeros(15);
+            bus2.AgregarPasajeros(9);
+
+            
+            central.AgregarBus(bus1);
+            central.AgregarBus(bus2);
+
+            
+            central.MostrarReporte();
+        }
+    }
+}﻿
